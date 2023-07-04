979bab1aa723a87943da4523027bc3b9785dc39e
Carlos Sullaez <jeraldy.sullaez@agetic.gob.bo>
2023-07-04 09:26:57 -0400

    feat: :rocket: Adicion de init.lua en configuracion

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- package.path = package.path .. ";/home/carlossullaez/dotfiles/lua/?.lua"
+-- require("/home/carlossullaez/.config/nvim/lua/main.lua")
+require("main")
+-- require("config.options")
+-- require("lua.config.main")
